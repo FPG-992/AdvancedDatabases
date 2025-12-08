1d0074216db6d236edb3bd5e2a55f0575226ca68
FPG-992 <78109143+FPG-992@users.noreply.github.com>
2025-12-09 01:20:21 +0200

    SH fix v2

--- run_git.sh
+++ run_git.sh
@@ -1,2 +1,2 @@
 docker compose up -d   
-docker compose exec -it spark-master bash -c "cd /app && ./run_all.sh"
+docker compose exec -it spark-master bash -c "cd /app && chmod +x ./run_all.sh && ./run_all.sh"
